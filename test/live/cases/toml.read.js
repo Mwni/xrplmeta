bb8910ebcf036a548bf40968c9ecacfeda387cd9
Mwni <manuel@tigmar.de>
2024-11-09 22:22:46 +0100

    use generic toml fetching function from domains crawler

--- test/live/cases/toml.read.js
+++ test/live/cases/toml.read.js
@@ -1,25 +1,17 @@
 import log from '@mwni/log'
 import { parse as parseXLS26 } from '@xrplkit/xls26'
 import { createFetch } from '../../../src/lib/fetch.js'
+import { fetchToml } from '../../../src/crawl/crawlers/domains.js'
 
 
 export default async ({ config, args }) => {
-	let tomlUrl = args._[1]
+	let domain = args._[1]
 	let fetch = createFetch()
 
-	if(!tomlUrl){
-		throw new Error(`no toml url provided. use: npm livetest read.toml [url]`)
-	}
+	if(!domain)
+		throw new Error(`no domain provided. use: npm livetest toml.read [domain]`)
 
-	log.info(`fetching ${tomlUrl}`)
-
-	let { status, data } = await fetch(tomlUrl)
-	
-	if(status !== 200){
-		throw new Error(`HTTP ${status}: ${tomlUrl}`)
-	}
-
-	let xls26 = parseXLS26(data)
+	let xls26 = await fetchToml({ domain, fetch })
 
 	log.info(`parsed xls26:\n`, xls26)
 }
